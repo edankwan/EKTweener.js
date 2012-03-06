8ceb750e97ad4a9c924812430af54dfecff53e5d
Edan Kwan <nemxin2@yahoo.com.hk>
2012-03-06 16:07:55 -0500

    added example of easing and events

--- examples/js/ui.js
+++ examples/js/ui.js
@@ -1,18 +1,20 @@
 ui = (function(){
+    
     var app = {
-        CANVAS_WIDTH: 580,
-        CANVAS_HEIGHT: 380,
-        IMG_WIDTH: 128,
-        IMG_HEIGHT:128
     }
     
-    var _viewSource;
     
     $(document).ready(function(){
-        _viewSource = $(".view-source");
-        _viewSource.click(function(){
-            window.open("view-source:" + window.location.href, "view-source", "width=720,height: 580");
+        $.ajax({
+            url : "menu.html",
+            success : function (data) {
+                $("#menu").html(data);
+            },
+            error: function(){
+                $("#menu").remove();
+            }
         });
+        
     })
     
     return app;
